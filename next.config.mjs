ee1b5001865881df3b992750be8240747d92e7c4
samdobson28 <134799184+samdobson28@users.noreply.github.com>
2024-12-15 18:43:19 -0500

    netlify iter 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Helps detect potential issues in your app
+    images: {
+      domains: [], // Add any external image domains here if you're using the `next/image` component
+    },
+    output: "standalone", // Recommended for serverless deployment like Netlify
+  };
+  
+  export default nextConfig;
+  
